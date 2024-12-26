cb9d3b49c71ebdafb2c6e55338d131cf5d0592e9
DayOS9 <velozity9@gmail.com>
2024-12-26 00:06:54 -0700

    background script now stores state of darkmode

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,15 +1,29 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("Received request: ", request);
+console.log("checking if everything is okay");
+// on start up, create a new var (if it doesnt exist) to store state
+// if an entry already exists, just skip
+async function setState() {
+  try {
+    let state = await browser.storage.local.get("Dark");
+    console.log("An entry exists!");
+  } catch (error) {
+    console.log(
+      "No entry was found; Will be creating an entry with default value",
+    );
+    await browser.storage.local.set({ Dark: 0 });
+  }
+}
 
-  if (request.greeting === "hello")
-    return Promise.resolve({ farewell: "goodbye" });
-});
-
-browser.runtime.onMessage.addListener((message, sender) => {
+browser.runtime.onMessage.addListener(async (message) => {
+  await setState();
   if (message.action === "toggle") {
-    // Send the message to the active tab's content script
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-      browser.tabs.sendMessage(tabs[0].id, message);
-    });
+    // reflect the change in the local storage
+    let state = await browser.storage.local.get("Dark");
+    if (state.Dark === 0) {
+      console.log("Dark mode is now enabled");
+      await browser.storage.local.set({ Dark: 1 });
+    } else {
+      console.log("Dark mode is now disabled");
+      await browser.storage.local.set({ Dark: 0 });
+    }
   }
 });
