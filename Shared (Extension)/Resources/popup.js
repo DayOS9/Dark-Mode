660a01c3c121bee5d7126dfaccaa7a2880a20d67
DayOS9 <velozity9@gmail.com>
2024-12-27 23:41:12 -0700

    pop up will now remember the last state, and appear with correct colors accordingly

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  // check the last saved state and update accordingly
+  try {
+    let state = await browser.storage.local.get("Dark");
+    if (state.Dark === 1) {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("This is probably the first time it is being ran");
+  }
   const button = document.getElementById("toggleButton");
   button.addEventListener("click", () => {
     var element = document.body;
